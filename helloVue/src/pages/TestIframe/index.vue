46a8e8ad557ecef34d84586648f9b30332fabc68
林志勇 <linzhiyong@changingedu.com>
2020-04-22 13:50:28 +0800

    iframe

--- helloVue/src/pages/TestIframe/index.vue
+++ helloVue/src/pages/TestIframe/index.vue
@@ -1,8 +1,14 @@
 <template>
-  <div>
-    pageA
-    <router-link to="/pageb">go to pageb</router-link>
-    <!-- <iframe src="http://localhost:8080/pageb" frameborder="0" ref="iframe"></iframe> -->
+  <div class="container">
+    <div class="nav">nav</div>
+    <div class="content">
+      <div class="content-header">content-header</div>
+      <div class="iframe-contanier">
+        <iframe src="http://localhost:8081/pageb" frameborder="0" ref="iframe"></iframe>
+      </div>
+      <div class="content-footer">content-footer</div>
+    </div>
+    <!-- <router-link to="/pageb">go to pageb</router-link> -->
   </div>
 </template>
 
@@ -25,5 +31,47 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+.container {
+  width: 100%;
+  height: 100%;
+  padding: 90px 0;
+  background-color: cornflowerblue;
+  display: flex;
+  .nav {
+    width: 100px;
+    flex-shrink: 0;
+    background-color: darkcyan;
+  }
+  .content {
+    flex-grow: 1;
+    background-color: darkkhaki;
+    width: 100%;
+    height: 100%;
+    position: relative;
+    padding: 90px 0 ;
+    .iframe-contanier {
+      width: 100%;
+      height: 100%;
+    }
+    .content-header {
+      position: absolute;
+      top: 0;
+      width: 100%;
+      height: 90px;
+      background-color: darksalmon;
+    }
+    .content-footer {
+      position: absolute;
+      bottom: 0;
+      width: 100%;
+      height: 90px;
+      background-color: darksalmon;
+    }
+  }
+  iframe {
+    height: 100%;
+    width: 100%;
+    background-color: cornsilk;
+  }
+}
 </style>
