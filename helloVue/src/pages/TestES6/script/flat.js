95b4f13dac18bb008a451726a8b02ea16fa6eeda
lawl1et <lawlietlzy@foxmail.com>
2020-07-15 01:02:42 +0800

    数组拍平

--- helloVue/src/pages/TestES6/script/flat.js
+++ helloVue/src/pages/TestES6/script/flat.js
@@ -1,10 +1,25 @@
-function flat (arr) {
-  let hasArr = arr.some(item => Array.isArray(arr))
+const flatArray = (arr) => {
+  if (!Array.isArray(arr)) return arr
+  const hasArr = arr.some(item => Array.isArray(item))
   if (!hasArr) return arr
-  let result = []
-  result = Array.prototype.concat.apply([], arr)
-
-  return flat(result)
+  return flatArray(Array.prototype.concat.apply([], arr))
 }
 
-export default flat
+const flatArray1 = (arr) => {
+  if (!Array.isArray(arr)) return arr
+  while (arr.some(item => Array.isArray(item))) {
+    arr = Array.prototype.concat.apply([], arr)
+  }
+  return arr
+}
+
+const flatArray2 = (arr) => {
+  if (!Array.isArray(arr)) return arr
+  return arr.toString().split(',').map((val) => parseInt(val))
+}
+
+const arr = [1, [2, 3], [[[[[[[7]]]]]]]]
+
+console.log('flatArray', flatArray(arr))
+console.log('flatArray1', flatArray1(arr))
+console.log('flatArray2', flatArray2(arr))
