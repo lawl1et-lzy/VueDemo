827932d5540d6b7264882ef5d50e5ac0b48ecdad
linzhiyong <zy.lin@trip.com>
2021-03-10 21:41:43 +0800

    add promiseRetry

--- helloVue/src/pages/TestES6/script/promiseRetry.js
+++ helloVue/src/pages/TestES6/script/promiseRetry.js
@@ -1,106 +1,44 @@
 /* eslint-disable */
-/**
- * 通过状态机
- */
-function createGenerator (fn, times) {
-  return function (...arg) {
-    return function *() {
-      // yield new Promise((resolve, reject) => {
-      //   setTimeout(() => {
-      //     reject(1111)
-      //   }, 2000)
-      // })
-      // yield new Promise((resolve, reject) => {
-      //   setTimeout(() => {
-      //     reject(2222)
-      //   }, 1000)
-      // })
-      // yield fn(...arg)
-      for (let i = 0; i < times; i++) {
-        yield fn(...arg)
-      }
-    }
-  }
-}
-function promiseRetry (fn, times) {
-  return (...args) => {
-    return new Promise((resolve, reject) => {
-      let task = createGenerator(fn, times)(...args)() // 启动任务
-      let result = task.next()
-      function step () {
-        if (!result.done) {
-          result.value.then(res => {
-            resolve(res)
-          }).catch(error => {
-            console.log('step ', error)
-            result = task.next()
-            step()
-          })
-        } else {
-          reject(`已经重试${times}次`)
-        }
-      }
-      step()
-    })
-  }
-}
+function promiseRetry(fn, handleFnRes, times = 3, delay = 0) {
+    return (...args) => {
+        return new Promise(async (resolve) => {
+            const retryFn = async (fn, times) => {
+                if (times) {
+                    let fnRes = ''
+                    let res = ''
 
-/**
- * Promise
- */
-const promiseRetry = (fn, times) => {
-  return new Promise((resolve, reject) => {
-    let error
-    const retry = (fn, times) => {
-      if (times) {
-        console.log('times', times)
-        fn().then(res => {
-          resolve(res)
-        }).catch(err => {
-          error = err
-          retry(fn, --times)
+                    try {
+                        fnRes = await fn.apply(this, args)
+                        res = (typeof handleFnRes === 'function') ? handleFnRes.apply(this, [fnRes]) : fnRes
+                    } catch (error) {
+                        return resolve({errorCode: 0, errorMsg: error, leftTimes: times})
+                    }
+
+                    if(res) {
+                        return resolve({errorCode: 1, leftTimes: times, data: res})
+                    } else {
+                        setTimeout(() => {
+                            retryFn(fn, --times)
+                        }, delay);
+                    }
+                } else {
+                    return resolve({errorCode: 1, leftTimes: times})
+                }
+            }
+
+            retryFn(fn, times)
         })
-      } else {
-        reject(error)
-      }
     }
-    retry(fn, times)
-  })
 }
 
-/**
- * await
- */
-function promiseRetry(fn, times) {
-  return (...args) => {
+function fn() {
     return new Promise((resolve, reject) => {
-      let error
-      try {
-        const retry = async (fn, times) => {
-          if (times) {
-            const res = await fn.apply(this, args)
-            resolve(res)
-          } else {
-            reject(error)
-          }
-        }
-        retry(fn, times)
-      } catch (error) {
-        error = error
-        retry(fn, times--)
-      }
+        const data = Math.random()
+        console.log('data', data);
+        data < 0.3 ? resolve(1) : resolve(2)
     })
-  }
 }
 
-// 示例
-const fn = () => {
-  return new Promise((resolve, reject) => {
-    Math.random() < 0.33 ? resolve(1) : reject(2)
-  })
-}
-promiseRetry(fn, 3).then(res => {
-  console.log('res', res)
-}).catch(err => {
-  console.log('err', err)
-})
+// const res = await this.promiseRetry(this.fn, (fnRes) => {
+//     return fnRes === 1
+// }, 10, 2000)()
